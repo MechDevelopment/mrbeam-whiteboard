ad58177fa8d437f96a7bb00d7b4e619c676dfb17
divasilevski <divasilevski@gmail.com>
2020-03-30 18:24:51 +0300

    btn created

--- src/components/v-tools.vue
+++ src/components/v-tools.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <div class="tools" :style="left_size">
-      <v-btn left dark>1</v-btn>
-      <v-btn left dark>2</v-btn>
+    <!-- LEFT -->
+    <div class="tools" :style="left_style">
+      <v-btn @click.prevent="clickPen" :color="color_pen" large icon>
+        <v-icon>mdi-pen</v-icon>
+      </v-btn>
+      <v-btn @click.prevent="clickEraser" :color="color_eraser" large icon>
+        <v-icon>mdi-eraser</v-icon>
+      </v-btn>
     </div>
-    <div class="tools" :style="right_size">
-      <v-btn left dark>3</v-btn>
-      <v-btn left dark>4</v-btn>
+
+    <!-- RIGHT -->
+    <div class="tools" :style="right_style">
+      <v-btn @click.prevent="clickUpload" color="#110f0f" large icon>
+        <v-icon>mdi-upload</v-icon>
+      </v-btn>
+      <v-btn @click.prevent="clickClear" color="#110f0f" large icon>
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
     </div>
   </div>
 </template>
@@ -20,8 +31,10 @@ export default {
   },
 
   data: () => ({
-    left_size: undefined,
-    right_size: undefined
+    color_pen: "#110f0f",
+    color_eraser: "#110f0f",
+    left_style: undefined,
+    right_style: undefined
   }),
 
   created() {
@@ -38,8 +51,8 @@ export default {
     onResize() {
       const left = (window.innerWidth - this.size) / 2 + 5;
       const bottom = (window.innerHeight - this.size) / 2 + 5;
-      this.left_size = `left: ${left}px; ` + `bottom: ${bottom}px; `;
-      this.right_size = `right: ${left}px; ` + `bottom: ${bottom}px; `;
+      this.left_style = `left: ${left}px; ` + `bottom: ${bottom}px; `;
+      this.right_style = `right: ${left}px; ` + `bottom: ${bottom}px; `;
     }
   }
 };
